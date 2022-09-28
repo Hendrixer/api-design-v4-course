0269e222ab7ecad350306926c16c10fbcc019901
Scott Moss <willscottmoss@gmail.com>
2022-09-28 15:43:53 -0500

    auth handlers

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import router from './router'
 import morgan from 'morgan'
 import cors from 'cors'
 import { protect } from './modules/auth'
+import { createNewUser, signin } from './handlers/user'
 
 const app = express()
 
@@ -18,5 +19,8 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api', protect, router)
+
+app.post('/user', createNewUser)
+app.post('/signin', signin)
 
 export default app
